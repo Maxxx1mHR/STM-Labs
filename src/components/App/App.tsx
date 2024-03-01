2e308565ac0524f37834c1b509a59a10a2b00140
Maxim Pudeev <maksim.pudeev@bk.ru>
2024-03-01 22:11:41 +0300

    chore: delete default unused project files. add normilize and default styles

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <section className="main container">
+      <h1>Test assignment for the position of Junior Frontend Developer</h1>
+    </section>
+  );
+};
