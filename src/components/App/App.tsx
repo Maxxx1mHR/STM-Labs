a903934bead61e75c45fc07f19f9da043b392c55
Maxim Pudeev <maksim.pudeev@bk.ru>
2024-03-01 23:02:45 +0300

    feat: add users API with RTK integration

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useGetUsersQuery } from '../../redux/usersApi';
+import { RootState } from '../../redux/store';
+
 export const App = () => {
+  const dispatch = useDispatch();
+
+  const { data: usersList, error, isLoading } = useGetUsersQuery({ count: 15 });
+
+  console.log(usersList?.results);
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <section className="main container">
       <h1>Test assignment for the position of Junior Frontend Developer</h1>
---
+++ src/redux/usersApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IUser } from '../types';
+
+const _api = 'https://randomuser.me/api/';
+
+interface IResponse {
+  results: [IUser];
+}
+
+export const usersApi = createApi({
+  reducerPath: 'usersApi',
+  baseQuery: fetchBaseQuery({ baseUrl: `${_api}` }),
+  endpoints: (build) => ({
+    getUsers: build.query<IResponse, { count: number }>({
+      query: ({ count }) => `?results=${count}`,
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersApi;
