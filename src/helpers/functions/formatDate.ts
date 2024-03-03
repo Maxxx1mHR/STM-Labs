0e5b65d57395085ecef9a411b198204476aa5078
Maxim Pudeev <maksim.pudeev@bk.ru>
2024-03-03 18:03:09 +0300

    feat: add bunlde stmlabs project

--- src/helpers/functions/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  return `${day}.${month}.${year}`;
-};
--- src/helpers/validation/checkInputString.ts
+++
@@ -1,5 +0,0 @@
-const regExp = /^[\p{L}\s]*$/u;
-
-export const checkInputString = (inputString: string) => {
-  return regExp.test(inputString);
-};
